b921ddeadfff3b575b20d4eb47fab8219f2f18ad
Stuart Gill <stuart.a.gill@gmail.com>
2019-03-01 14:09:15 -0800

    retrieves and displays posts from jsonplaceholder.com

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Posts from "./components/Posts";
 
 class App extends Component {
   render() {
@@ -8,9 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Posts />
           <a
             className="App-link"
             href="https://reactjs.org"
