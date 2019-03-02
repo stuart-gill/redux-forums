fbc4eb18ba03ded14dc0695db593a441b87e5357
Stuart Gill <stuart.a.gill@gmail.com>
2019-03-01 17:55:09 -0800

    adds redux, provider and createstore added to app.js, everything working but redux isnt actually being used yet

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import logo from "./logo.svg";
 import "./App.css";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
+import {Provider} from 'react-redux';
+import { createStore , applyMiddleware } from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Posts />
-          <hr/>
-          <PostForm />
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Posts />
+            <hr/>
+            <PostForm />
 
-        </header>
-      </div>
+          </header>
+        </div>
+      </Provider>
     );
   }
 }
